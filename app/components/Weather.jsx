d7a20e3954db6afffb9801de1f248682d23d7f45
Jose Pablo Madrigal <jose.pablo.madrigal@firstfactory.com>
2016-09-07 13:56:30 -0600

    Fixed server.js

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -4,43 +4,44 @@ var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
-  getInitialState: function(){
+  getInitialState: function () {
     return {
       isLoading: false
     }
   },
-  handleSearch: function(location){
+  handleSearch: function (location) {
     var that = this;
+
     this.setState({isLoading: true});
 
-    openWeatherMap.getTemp(location).then(function(temp){
+    openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
         temp: temp,
         isLoading: false
       });
-
-    },function(errorMessage){
+    }, function (errorMessage) {
       that.setState({isLoading: false});
       alert(errorMessage);
     });
   },
   render: function () {
-    var {isLoading,temp, location} = this.state;
-    function renderMessage(){
-      if(isLoading){
+    var {isLoading, temp, location} = this.state;
+
+    function renderMessage () {
+      if (isLoading) {
         return <h3>Fetching weather...</h3>;
-      }else if(temp && location){
-        return <WeatherMessage temp={temp} location = {location} />;
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location}/>;
       }
     }
+
     return (
       <div>
         <h3>Weather Component</h3>
-        <WeatherForm onSearch = {this.handleSearch}/>
+        <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
       </div>
-
     )
   }
 });
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,18 +1,9 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-
-  render: function(){
-
-  }
-});
-
 var WeatherMessage = ({temp, location}) => {
   return (
-    <div>
-      <h3>It's it {temp} in {location}.</h3>
-    </div>
-  );
-}
+    <h3>It's it {temp} in {location}.</h3>
+  )
+};
 
 module.exports = WeatherMessage;
