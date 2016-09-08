737773fd4c27a92d578eabcb058f89b3accf9c0f
Jose Pablo Madrigal <jose.pablo.madrigal@firstfactory.com>
2016-09-08 10:50:26 -0600

    Styles added on examples jsx

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -40,11 +40,4 @@ var Nav = React.createClass ({
   }
 });
 
-// <div>
-//   <h2>Nav Component</h2>
-//   <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-//   <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-//   <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
-// </div>
-
 module.exports = Nav;
